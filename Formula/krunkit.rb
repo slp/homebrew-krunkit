157034d6f49a5c5045ae72e0be1685ba56edde9e
Sergio Lopez <slp@redhat.com>
2024-10-04 12:38:19 +0200

    Fix krunkit formula and bottle

--- Formula/krunkit.rb
+++ Formula/krunkit.rb
@@ -4,10 +4,11 @@ class Krunkit < Formula
   url "https://github.com/containers/krunkit/archive/refs/tags/v0.1.3.tar.gz"
   sha256 "bc8214e00913c560b98a4b4d3d7da8052eeb78dc9e13a955e3018bee3e77d5fe"
   license "Apache-2.0"
+  version "0.1.3b"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krunkit/master/bottles"
-    sha256 cellar: :any, arm64_sonoma: "f0e766395b0a1b2884bcd91383291095872723257823909af5b2d3c4c308ea22"
+    sha256 cellar: :any, arm64_sequoia: "947a73dab090236984fa3c7409eccbfb1712c9da67d59bd6e95c15b68b2ef4fa"
   end
 
   depends_on "rust" => :build
@@ -16,7 +17,10 @@ class Krunkit < Formula
   depends_on "libkrun-efi"
 
   def install
+    homebrew_lib = ENV["HOMEBREW_PREFIX"] + "/lib"
     system "make"
+    system "install_name_tool", "-add_rpath", homebrew_lib, "target/release/krunkit"
+    system "codesign", "--entitlements", "krunkit.entitlements", "--force", "-s", "-", "target/release/krunkit"
     bin.install "target/release/krunkit"
   end
 
