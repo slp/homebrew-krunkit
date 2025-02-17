d400de4fa8c89014b2908471c2f5c91025b50f75
Sergio Lopez <slp@redhat.com>
2025-02-17 11:30:14 +0100

    Include formula for molten-vk-krunkit

--- Formula/virglrenderer.rb
+++ Formula/virglrenderer.rb
@@ -1,20 +1,20 @@
 class Virglrenderer < Formula
   desc "VirGL virtual OpenGL renderer"
   homepage "https://gitlab.freedesktop.org/virgl/virglrenderer"
-  url "https://gitlab.freedesktop.org/slp/virglrenderer/-/archive/0.10.4b-krunkit/virglrenderer-0.10.4b-krunkit.tar.gz"
-  sha256 "436761e1154cf95d053172f86ff994ba2210158ee5cf03a584d95dd59558e21b"
+  url "https://gitlab.freedesktop.org/slp/virglrenderer/-/archive/0.10.4c-krunkit/virglrenderer-0.10.4c-krunkit.tar.gz"
+  sha256 "b905e4900467757212c82b00c7ef98b427ab0ecfecfce6701b29d0fe09de4aa4"
   license "MIT"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krunkit/master/bottles"
-    sha256 cellar: :any, arm64_sequoia: "407c8b8135894aa28307e5efebc230fcf5752d2f699d402180634bbcddd8c4b0"
+    sha256 cellar: :any, arm64_sequoia: "71dd0008d06646846a253d87e4ff5201275bf4d0ba2215b5461b22a0844316b5"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "libepoxy"
-  depends_on "molten-vk"
+  depends_on "molten-vk-krunkit"
 
   def install
     args = %w[
