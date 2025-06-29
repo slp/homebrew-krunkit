38b642e60aa616e0dae204b26bcf0538ce2bb5fe
Sergio Lopez <slp@redhat.com>
2025-06-29 20:11:50 +0200

    Update krunkit to 0.2.2

--- Formula/krunkit.rb
+++ Formula/krunkit.rb
@@ -1,13 +1,13 @@
 class Krunkit < Formula
   desc "A CLI tool to start Linux KVM or macOS Hypervisor framework virtual machines using the libkrun platform."
   homepage "https://github.com/containers/krunkit"
-  url "https://github.com/containers/krunkit/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "a20d435c1f26250fb0a1239f28f6552e0cc065afce004f7e048a3588e545ff3a"
+  url "https://github.com/containers/krunkit/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "750f70cb6ae97b2bc34b3d167ff3db51bd2e2ce6feb2d443dc555c9c7f3432f4"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krunkit/master/bottles"
-    sha256 cellar: :any, arm64_sequoia: "bc867f6b4999544fa0b541dca53635feb6399eeb74cc85c661006035ab1c1c95"
+    sha256 cellar: :any, arm64_sequoia: "0ea68a5ebaece0d245d852e1a5f23b089d044c2063b4ca265382c0eb3ef0e401"
   end
 
   depends_on "rust" => :build
