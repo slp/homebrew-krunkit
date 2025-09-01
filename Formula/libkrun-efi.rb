50801667e7d46c413ec6d0278dc6598e0fae0a4c
Sergio Lopez <slp@redhat.com>
2025-09-01 16:32:37 +0200

    Update libkrun-efi to 1.15.1

--- Formula/libkrun-efi.rb
+++ Formula/libkrun-efi.rb
@@ -1,13 +1,13 @@
 class LibkrunEfi < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/containers/libkrun"
-  url "https://github.com/containers/libkrun/archive/refs/tags/v1.15.0.tar.gz"
-  sha256 "1ed32b260654dd5892a12e86c2e3aa7bf30f95197bf2497f009bb8b1bd37854d"
+  url "https://github.com/containers/libkrun/archive/refs/tags/v1.15.1.tar.gz"
+  sha256 "d01240bdd567de81ef8da9776595633f6fade694721622295a033c2c332a544f"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krunkit/master/bottles"
-    sha256 cellar: :any, arm64_sequoia: "52f2b3d8a49c84ae284b42df5d0b2ae055551b7064f08faedf3e17f643d11470"
+    sha256 cellar: :any, arm64_sequoia: "ace439fcb129568e8ca455d3518ff9c4dc0f48c38f746be828f093593e35e6d5"
   end
 
   depends_on "rust" => :build
