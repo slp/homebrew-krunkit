8a8177195e8098525ced1f1f8b13b34d1bd6f19e
Sergio Lopez <slp@redhat.com>
2025-10-28 11:59:53 +0100

    Update libkrun-efi to 1.16.0

--- Formula/libkrun-efi.rb
+++ Formula/libkrun-efi.rb
@@ -1,13 +1,13 @@
 class LibkrunEfi < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/containers/libkrun"
-  url "https://github.com/containers/libkrun/archive/refs/tags/v1.15.1.tar.gz"
-  sha256 "d01240bdd567de81ef8da9776595633f6fade694721622295a033c2c332a544f"
+  url "https://github.com/containers/libkrun/archive/refs/tags/v1.16.0.tar.gz"
+  sha256 "04b6f01f44e263d168757ebcd9bc037d7c5836c1987a547c6b2fa5837abe1ab1"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krunkit/master/bottles"
-    sha256 cellar: :any, arm64_sequoia: "ace439fcb129568e8ca455d3518ff9c4dc0f48c38f746be828f093593e35e6d5"
+    sha256 cellar: :any, arm64_sequoia: "3f4f48595bed22bf942a28fc946ae6583c5665bf5e9d14768cf992bf8f8b9df4"
   end
 
   depends_on "rust" => :build
@@ -17,8 +17,9 @@ class LibkrunEfi < Formula
   depends_on "slp/homebrew-krunkit/virglrenderer"
 
   def install
-    system "make", "EFI=1", "GPU=1"
-    system "make", "EFI=1", "GPU=1", "PREFIX=#{prefix}", "install"
+    system "touch", "init/init"
+    system "make", "EFI=1", "GPU=1", "BLK=1", "NET=1"
+    system "make", "EFI=1", "GPU=1", "BLK=1", "NET=1", "PREFIX=#{prefix}", "install"
   end
 
   test do
